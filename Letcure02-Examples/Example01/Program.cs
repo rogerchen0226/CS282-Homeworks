cb0ad984a300147fc4d77e4efdeb0c40ec8c793c
rogerchen0226 <alba0226@hotmail.com>
2017-05-29 10:36:51 +0800

    第二次練習範例

---
+++ Letcure02-Examples/Example01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(0.1 + 0.2 == 0.3);    // round-off error
+            Console.WriteLine(0.25 + 0.25 == 0.5);
+            Console.WriteLine(1.2 + 1.3 == 2.5);
+            Console.WriteLine(0.1f + 0.2f == 0.3f); // ok
+        }
+    }
+}
---
+++ Letcure02-Examples/Example02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1 / 2);   // round-off error
+            Console.WriteLine(1.0 / 2.0 * 3.0);
+            Console.WriteLine(1.0 / 4.0);
+
+        }
+    }
+}
